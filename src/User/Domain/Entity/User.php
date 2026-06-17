5cd11fb6e944e33cb17560ec8a6d4e8245cc9525
Nathã Emanuel Gonçalves <nathaemanuel.eng@gmail.com>
2026-06-16 21:56:14 -0300

    feat(user): add user entity

---
+++ src/User/Domain/Entity/User.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Domain\Entity;
+
+use App\User\Domain\ValueObject\Email;
+use App\User\Domain\ValueObject\PasswordHash;
+use App\User\Domain\ValueObject\UserId;
+use App\User\Domain\ValueObject\UserName;
+
+final readonly class User
+{
+    private function __construct(
+        private UserId $id,
+        private UserName $name,
+        private Email $email,
+        private PasswordHash $passwordHash,
+    ) {
+    }
+
+    public static function create(
+        UserId $id,
+        UserName $name,
+        Email $email,
+        PasswordHash $passwordHash,
+    ): self {
+        return new self($id, $name, $email, $passwordHash);
+    }
+
+    public function id(): UserId
+    {
+        return $this->id;
+    }
+
+    public function name(): UserName
+    {
+        return $this->name;
+    }
+
+    public function email(): Email
+    {
+        return $this->email;
+    }
+
+    public function passwordHash(): PasswordHash
+    {
+        return $this->passwordHash;
+    }
+}
---
+++ tests/Unit/User/Domain/Entity/UserTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\User\Domain\Entity;
+
+use App\User\Domain\Entity\User;
+use App\User\Domain\ValueObject\Email;
+use App\User\Domain\ValueObject\PasswordHash;
+use App\User\Domain\ValueObject\UserId;
+use App\User\Domain\ValueObject\UserName;
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase
+{
+    public function testShouldCreateUser(): void
+    {
+        $id = UserId::fromString('550e8400-e29b-41d4-a716-446655440000');
+        $name = UserName::fromString('John Doe');
+        $email = Email::fromString('john.doe@example.com');
+        $passwordHash = PasswordHash::fromString(password_hash('StrongPassword123!', PASSWORD_ARGON2ID));
+
+        $user = User::create($id, $name, $email, $passwordHash);
+
+        self::assertSame($id, $user->id());
+        self::assertSame($name, $user->name());
+        self::assertSame($email, $user->email());
+        self::assertSame($passwordHash, $user->passwordHash());
+    }
+}
