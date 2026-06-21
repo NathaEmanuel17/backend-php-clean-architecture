2db18a999967929d79ceb7fa0bcd6bd5f08acceb
Nathã Emanuel Gonçalves <nathaemanuel.eng@gmail.com>
2026-06-21 17:36:13 -0300

    test(user): add get user by id integration test

---
+++ tests/Integration/User/Application/UseCase/GetUserByIdUseCaseTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\User\Application\UseCase;
+
+use App\Shared\Infrastructure\Persistence\PdoConnectionFactory;
+use App\User\Application\UseCase\GetUserByIdUseCase;
+use App\User\Domain\Entity\User;
+use App\User\Domain\ValueObject\Email;
+use App\User\Domain\ValueObject\PasswordHash;
+use App\User\Domain\ValueObject\UserId;
+use App\User\Domain\ValueObject\UserName;
+use App\User\Infrastructure\Repository\PostgresUserRepository;
+use PDO;
+use PHPUnit\Framework\TestCase;
+
+final class GetUserByIdUseCaseTest extends TestCase
+{
+    private PDO $pdo;
+
+    protected function setUp(): void
+    {
+        $this->pdo = PdoConnectionFactory::create();
+
+        $this->pdo->exec('DELETE FROM users');
+    }
+
+    public function testShouldGetUserFromDatabase(): void
+    {
+        $repository = new PostgresUserRepository(
+            $this->pdo
+        );
+
+        $user = User::create(
+            UserId::fromString(
+                '550e8400-e29b-41d4-a716-446655440000'
+            ),
+            UserName::fromString(
+                'John Doe'
+            ),
+            Email::fromString(
+                'john.doe@example.com'
+            ),
+            PasswordHash::fromString(
+                password_hash(
+                    'StrongPassword123!',
+                    PASSWORD_ARGON2ID
+                )
+            ),
+        );
+
+        $repository->save($user);
+
+        $useCase = new GetUserByIdUseCase(
+            $repository
+        );
+
+        $output = $useCase->execute(
+            '550e8400-e29b-41d4-a716-446655440000'
+        );
+
+        self::assertSame(
+            'John Doe',
+            $output->name
+        );
+
+        self::assertSame(
+            'john.doe@example.com',
+            $output->email
+        );
+    }
+}
